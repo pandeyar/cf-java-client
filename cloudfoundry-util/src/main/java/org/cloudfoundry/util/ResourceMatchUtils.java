163fbc7442d8fb1a04fac96bd89edcdec0145ac0
Ben Hale <bhale@pivotal.io>
2016-07-28 12:32:25 -0700

    [ci skip] Incremental commit. Please do not base any external work off of this commit.

--- cloudfoundry-util/src/main/java/org/cloudfoundry/util/ResourceMatchUtils.java
+++ cloudfoundry-util/src/main/java/org/cloudfoundry/util/ResourceMatchUtils.java
@@ -17,16 +17,122 @@
 package org.cloudfoundry.util;
 
 import org.cloudfoundry.client.CloudFoundryClient;
+import org.cloudfoundry.client.v2.resourcematch.ListMatchingResourcesRequest;
+import org.cloudfoundry.client.v2.resourcematch.ListMatchingResourcesResponse;
+import org.cloudfoundry.client.v2.resourcematch.Resource;
+import reactor.core.Exceptions;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.cloudfoundry.util.tuple.TupleUtils.function;
 
 public final class ResourceMatchUtils {
 
     private ResourceMatchUtils() {
     }
 
-    public static Mono<String> getMatchedResources(CloudFoundryClient cloudFoundryClient, Path application) {
+    public static Flux<ResourceMetadata> getMatchedResources(CloudFoundryClient cloudFoundryClient, Path application) {
+        return getResources(application)
+            .flatMap(ResourceMatchUtils::getMetadata)
+            .collectList()
+            .flatMap(metadata -> Flux.fromIterable(metadata)
+                .map(ResourceMatchUtils::toResource)
+                .as(ResourceMatchUtils::toListMatchingResourceRequest)
+                .then(request -> getMatchedHashes(cloudFoundryClient, request))
+                .flatMap(matchedHashes -> toMatchedResources(metadata, matchedHashes)));
+    }
+
+    private static Mono<String> getHash(Path resource) {
+        return Mono.just(FileUtils.getSha1(resource));
+    }
+
+    private static Mono<Set<String>> getMatchedHashes(CloudFoundryClient cloudFoundryClient, ListMatchingResourcesRequest request) {
+        return cloudFoundryClient.resourceMatch()
+            .list(request)
+            .map(ResourceMatchUtils::toMatchedHashes);
+    }
+
+    private static Mono<ResourceMetadata> getMetadata(Path resource) {
+        return Mono
+            .when(
+                getHash(resource),
+                getMode(resource),
+                Mono.just(resource),
+                getSize(resource)
+            )
+            .map(function(ResourceMetadata::new));
+    }
+
+    private static Mono<String> getMode(Path resource) {
+        return Mono.just(Integer.toOctalString(FileUtils.getUnixMode(resource)));
+    }
+
+    private static Flux<Path> getResources(Path root) {
+        try {
+            return Flux
+                .fromStream(Files.walk(FileUtils.normalize(root)))
+                .filter(Files::isRegularFile);
+        } catch (IOException e) {
+            throw Exceptions.propagate(e);
+        }
+    }
+
+    private static Mono<Long> getSize(Path resource) {
+        try {
+            return Mono.just(Files.size(resource));
+        } catch (IOException e) {
+            throw Exceptions.propagate(e);
+        }
+    }
+
+    private static Mono<ListMatchingResourcesRequest> toListMatchingResourceRequest(Flux<Resource> source) {
+        return source
+            .reduce(ListMatchingResourcesRequest.builder(), ListMatchingResourcesRequest.Builder::resource)
+            .map(ListMatchingResourcesRequest.Builder::build);
+    }
+
+    private static Set<String> toMatchedHashes(ListMatchingResourcesResponse response) {
+        return response.getResources().stream()
+            .map(Resource::getHash)
+            .collect(Collectors.toSet());
+    }
+
+    private static Flux<ResourceMetadata> toMatchedResources(List<ResourceMetadata> metadata, Set<String> matchedHashes) {
+        return Flux.fromStream(metadata.stream())
+            .filter(m -> matchedHashes.contains(m.hash));
+    }
+
+    private static Resource toResource(ResourceMetadata metadata) {
+        return Resource.builder()
+            .hash(metadata.hash)
+            .mode(metadata.mode)
+            .size(metadata.size)
+            .build();
+    }
+
+    private static final class ResourceMetadata {
+
+        private final String hash;
+
+        private final String mode;
+
+        private final Path resource;
+
+        private final Long size;
+
+        private ResourceMetadata(String hash, String mode, Path resource, Long size) {
+            this.hash = hash;
+            this.mode = mode;
+            this.resource = resource;
+            this.size = size;
+        }
 
     }
 
